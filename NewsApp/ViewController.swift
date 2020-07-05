15d169c9e86863f5ff2591015b15c8833f2a8aa3
Muhammad Abu Mandour <muhammadabumandour@outlook.com>
2020-07-05 15:56:12 +0200

    init projects

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Muhammad Abu Mandour on 7/5/20.
+//  Copyright © 2020 iHorizons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
