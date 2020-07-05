bc9e08390a85af18015018deb4bb5b1902696650
Muhammad Abu Mandour <muhammadabumandour@outlook.com>
2020-07-05 18:20:28 +0200

    add repo service

--- NewsApp/Repositories/HomeRepoService.swift
+++ NewsApp/Repositories/HomeRepoService.swift
@@ -8,35 +8,36 @@
 
 import Foundation
 import Alamofire
+	
 
 class HomeRepoService
 {
     class func getHomeRepo(pageNum : String , completion : @escaping( _ error: Error? , _ success: Bool, _ data :[HomeApi]?)->Void)
     {
-//        let URL = AppUrls.home + pageNum
-//        Alamofire.request(URL).responseArray{ (response: DataResponse<[HomeApi]>) in
-//            if response.error != nil
-//            {
-//                completion(response.error,false, response.result.value)
-//            }
-//            else
-//            {
-//                completion(response.error,true, response.result.value)
-//            }
-//        }
+        let URL = AppUrls.home + pageNum
+        Alamofire.request(URL).responseArray{ (response: DataResponse<[HomeApi]>) in
+            if response.error != nil
+            {
+                completion(response.error,false, response.result.value)
+            }
+            else
+            {
+                completion(response.error,true, response.result.value)
+            }
+        }
     }
     class func getMenuRepo( completion : @escaping( _ error: Error? , _ success: Bool, _ data :[MenuApi]?)->Void)
     {
-//        let URL = AppUrls.menu
-//        Alamofire.request(URL).responseArray{ (response: DataResponse<[MenuApi]>) in
-//            if response.error != nil
-//            {
-//                completion(response.error,false, response.result.value)
-//            }
-//            else
-//            {
-//                completion(response.error,true, response.result.value)
-//            }
-//        }
+        let URL = AppUrls.menu
+        Alamofire.request(URL).responseArray{ (response: DataResponse<[MenuApi]>) in
+            if response.error != nil
+            {
+                completion(response.error,false, response.result.value)
+            }
+            else
+            {
+                completion(response.error,true, response.result.value)
+            }
+        }
     }
 }
